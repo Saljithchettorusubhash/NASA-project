7a31e9e8e066260e3ae467c9e88ad0ddcddb6b50
SALJITH <saljithsubash661@gmail.com>
2024-10-14 10:11:55 +0100

    changed verscel configuration

--- backend/Config/Config.js
+++ backend/Config/Config.js
@@ -2,14 +2,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const Config = {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 3311,
+    NODE_ENV: process.env.NODE_ENV || 'production',
+    PORT: process.env.PORT || 3331,
     REDIS_HOST: process.env.REDIS_HOST || '127.0.0.1',
     REDIS_PORT: process.env.REDIS_PORT || 6379,
-    NASA_API_KEY: process.env.NASA_API_KEY ,
-    ALLOWED_ORIGIN:process.env.ALLOWED_ORIGIN || 'https://nasa-project-szum-aqbi57o4a-saljiths-projects.vercel.app',
-
-
+    NASA_API_KEY: process.env.NASA_API_KEY || 'DEMO_KEY',
+    ALLOWED_ORIGIN:process.env.ALLOWED_ORIGIN || 'https://nasa-project-szum.vercel.app',
 
 };
 
