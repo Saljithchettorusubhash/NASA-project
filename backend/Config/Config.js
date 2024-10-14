1184c9e0ccf046d7ce9e5a0e19b91617d27039a0
SALJITH <saljithsubash661@gmail.com>
2024-10-14 09:06:19 +0100

    fixed the issue

--- backend/Config/Config.js
+++ backend/Config/Config.js
@@ -3,11 +3,11 @@ dotenv.config();
 
 export const Config = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    PORT: process.env.PORT || 3331,
+    PORT: process.env.PORT || 5000,
     REDIS_HOST: process.env.REDIS_HOST || '127.0.0.1',
     REDIS_PORT: process.env.REDIS_PORT || 6379,
-    NASA_API_KEY: process.env.NASA_API_KEY || 'DEMO_KEY',
-    ALLOWED_ORIGIN:process.env.ALLOWED_ORIGIN || 'ttp://localhost:5173',
+    NASA_API_KEY: process.env.NASA_API_KEY ,
+    ALLOWED_ORIGIN:process.env.ALLOWED_ORIGIN || 'https://nasa-project-szum.vercel.app',
 
 
 
