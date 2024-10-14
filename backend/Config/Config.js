f105ee9fc873409bb1ee7efbb5f3adaef2bf79fd
SALJITH <saljithsubash661@gmail.com>
2024-10-14 09:19:56 +0100

    changed the origin

--- backend/Config/Config.js
+++ backend/Config/Config.js
@@ -7,7 +7,7 @@ export const Config = {
     REDIS_HOST: process.env.REDIS_HOST || '127.0.0.1',
     REDIS_PORT: process.env.REDIS_PORT || 6379,
     NASA_API_KEY: process.env.NASA_API_KEY ,
-    ALLOWED_ORIGIN:process.env.ALLOWED_ORIGIN || 'https://nasa-project-szum.vercel.app',
+    ALLOWED_ORIGIN:process.env.ALLOWED_ORIGIN || 'https://nasa-project-szum-aqbi57o4a-saljiths-projects.vercel.app',
 
 
 
