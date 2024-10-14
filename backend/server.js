2ec695445901703cf6bc65d377c20b87eb28f65e
SALJITH <saljithsubash661@gmail.com>
2024-10-14 10:21:04 +0100

    changed the port

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,15 @@ import { MiddlewareLoader } from "./loader/middleare.loader.js";
 import { RoutesLoader } from "./loader/routes.loader.js";
 import { Config } from './Config/Config.js';
 
+// Use the PORT environment variable provided by Render or fall back to 3331 if not set
+const port = process.env.PORT || Config.PORT || 3331;  
 
-const port = Number(Config.port) || 3311;
 const app = ExpressLoader.init();
 const version = 'v1';
-RoutesLoader.initRoutes(app,version);
+RoutesLoader.initRoutes(app, version);
 
 MiddlewareLoader.init(app);
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server started at port ${port} in ${Config.NODE_ENV} mode`);
-})
+});
